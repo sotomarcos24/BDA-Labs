a645f04838748b97469a9722ea14fbf7fd38982f
Marcos <sotomarcos24@gmail.com>
2026-04-27 21:00:25 +0100

    WIP session 1 part 2

---
+++ session1/solutions/exercise-01-02.py
@@ -0,0 +1,9 @@
+from exercise_01_02_lib import my_len
+
+print(my_len([10, 20, 30]))
+
+data = [10, 20, 30, 40, 50]
+total = 0
+for i in data:
+    total += i
+print(total)
---
+++ session1/solutions/exercise_01_02_lib.py
@@ -0,0 +1,5 @@
+def my_len(data):
+    count = 0
+    for item in data:
+        count += 1
+    return count
